f4424fb6dc3754511b89bafce1411a055353db51
BorysGull <chayka2021@yahoo.com>
2023-12-19 18:50:59 +0200

    binary_tree

--- binary_tree.js
+++ binary_tree.js
@@ -1,1 +1,130 @@
+// const tree = {
+//   value: 8,
+//   left: {
+//     value: 6,
+//     left: null,
+//     right: null,
+//   },
+//   right: {
+//     value: 10,
+//     left: {
+//       value: 9,
+//       left: null,
+//       right: null,
+//     },
+//   },
+// };
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  add(value) {
+    const newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return;
+    }
+
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (newNode.value < currentNode.value) {
+        if (!currentNode.left) {
+          currentNode.left = newNode;
+          return;
+        }
+
+        currentNode = currentNode.left;
+      } else {
+        if (!currentNode.right) {
+          currentNode.right = newNode;
+          return;
+        }
+        currentNode = currentNode.right;
+      }
+    }
+  }
+
+  preOrder(node, callback) {
+    if (!node) {
+      return;
+    }
+    if (callback) {
+      callback(node);
+    }
+    this.preOrder(node.left, callback);
+    this.preOrder(node.right, callback);
+  }
+
+  inOrder(node, callback) {
+    if (!node) {
+      return;
+    }
+
+    this.inOrder(node.left, callback);
+    if (callback) {
+      callback(node);
+    }
+    this.inOrder(node.right, callback);
+  }
+  postOrder(node, callback) {
+    if (!node) {
+      return;
+    }
+
+    this.postOrder(node.left, callback);
+    this.postOrder(node.right, callback);
+    if (callback) {
+      callback(node);
+    }
+  }
+  traverseDFS(callback, method) {
+    if (method === "preOrder") {
+      return this.preOrder(this.root, callback);
+    }
+
+    if (method === "inOrder") {
+      return this.preOrder(this.root, callback);
+    }
+
+    return this.postOrder(this.root, callback);
+  }
+}
+const myTree = new BinaryTree();
+myTree.add(8);
+myTree.add(7);
+myTree.add(9);
+myTree.add(5);
+myTree.add(10);
+myTree.add(20);
+myTree.add(6);
+myTree.add(2);
+myTree.add(11);
+8;
+7;
+5;
+2;
+6;
+9;
+10;
+20;
+11;
+// myTree.traverseDFS((node) => {
+//   console.log(node.value);
+// }, "preOrder");
+// myTree.traverseDFS((node) => {
+//   console.log(node.value);
+// }, "inOrder");
+myTree.traverseDFS((node) => {
+  console.log(node.value);
+}, "postOrder");
